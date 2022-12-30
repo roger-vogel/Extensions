0b253ccd29aff8af7de62efa5770329b322f3262
Roger Vogel <roger@puldypartners.com>
2022-12-30 00:16:03 -0800

    Cleanup

--- Sources/Extensions/UITextFieldExtension.swift
+++ Sources/Extensions/UITextFieldExtension.swift
@@ -30,10 +30,10 @@ public extension UITextField{
             self.leftViewMode = .always
         }
 
-        func setRightPadding(_ amount: CGFloat = 10) {
+    func setRightPadding(_ amount: CGFloat = 10) {
 
-            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.bounds.size.height))
-            self.rightView = paddingView
-            self.rightViewMode = .always
-        }
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.bounds.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
 }
